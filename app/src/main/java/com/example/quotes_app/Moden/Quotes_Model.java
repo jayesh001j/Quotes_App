273834408374c83067ce55e205eef86b5e2d21fc
JA_YE_SH_001 <134709268+jayesh001j@users.noreply.github.com>
2025-05-19 15:07:16 +0530

    Update Quotes_Model.java

--- app/src/main/java/com/example/quotes_app/Moden/Quotes_Model.java
+++ app/src/main/java/com/example/quotes_app/Moden/Quotes_Model.java
@@ -3,29 +3,24 @@ package com.example.quotes_app.Moden;
 public class Quotes_Model {
 
     public Integer image;
+    public String Quotes;
 
-    public  String Quotes;
 
-    public Quotes_Model(Integer image, String Quotes){
+
+    public Quotes_Model(Integer image, String Quotes) {
         this.image = image;
         this.Quotes = Quotes;
-    }
 
-    public Quotes_Model(Integer image, String quotes, String s) {
-
-    }
-
-    public String getCategoryName() {
-
-
-        return null;
     }
 
     public int getImage() {
-        return 0;
+        return image;
     }
 
-    public int getQuotes() {
-        return 0;
+
+
+    public String getQuotes() {
+        return Quotes;
     }
+
 }
