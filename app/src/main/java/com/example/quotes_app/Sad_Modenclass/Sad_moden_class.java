ed95e24435d531776f8408e0ea5ac9f46d2aebff
JA_YE_SH_001 <134709268+jayesh001j@users.noreply.github.com>
2025-05-19 15:10:25 +0530

    Update Sad_moden_class.java

--- app/src/main/java/com/example/quotes_app/Sad_Modenclass/Sad_moden_class.java
+++ app/src/main/java/com/example/quotes_app/Sad_Modenclass/Sad_moden_class.java
@@ -1,7 +1,5 @@
 package com.example.quotes_app.Sad_Modenclass;
 
-import android.graphics.Bitmap;
-
 public class Sad_moden_class {
     public Integer Image_passing;
     public String Sad_name;
@@ -11,7 +9,21 @@ public class Sad_moden_class {
         this.Image_passing = Image_passing;
         this.Sad_name = Sad_name;
         this.Author_name = Author_name;
+
     }
 
+
+    public int getImage_passing() {
+        return Image_passing;
+    }
+
+    public String getSad_name() {
+        return Sad_name;
+    }
+
+
+    public String getAuthor_name() {
+        return Author_name;
+    }
 
 }
